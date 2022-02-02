5f41166db7904f2d331547815ac9974771cdab14
Dirga <dirgantoro.developer@gmail.com>
2022-02-02 14:50:12 +0700

    Auth service update

--- KANO.ESS/KANO.Api.Administration/Controllers/AdministrationController.cs
+++ KANO.ESS/KANO.Api.Administration/Controllers/AdministrationController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using KANO.Core.Lib;
 using KANO.Core.Lib.Helper;
+using KANO.Core.Model.Auth;
 using KANO.Core.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
+using System.IO;
+using System.Net;
+using KANO.Core.Lib.Extension;
+using KANO.Core.Model;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,6 +31,94 @@ namespace KANO.Api.Administration.Controllers
             Mongo = mongo;
             DB = Mongo.Database();     
             Configuration = conf;      
+        }
+
+        [HttpGet("getconfigpassword")]
+        public IActionResult GetConfigPassword()
+        {
+            try
+            {
+                return ApiResult<ConfigPassword>.Ok(DB.GetCollection<ConfigPassword>().Find(a => a.Published == 1).FirstOrDefault());
+            }
+            catch (Exception e)
+            {
+                return ApiResult<object>.Error(HttpStatusCode.BadRequest, $"Unable to get config :\n{Format.ExceptionString(e)}");
+            }
+        }
+        [HttpPost("updateconfigpassword")]
+        public IActionResult UpdateConfigPassword([FromForm] FamilyForm param)
+        {
+            try
+            {
+                DB.Save(JsonConvert.DeserializeObject<ConfigPassword>(param.JsonData));
+                return ApiResult<object>.Ok($"Config Password Has Been Updated");
+            }
+            catch (Exception e)
+            {
+                return ApiResult<object>.Ok($"Error Update Config Password :\n{e.Message}");
+            }
+        }
+
+        [HttpGet("getmobileversion")]
+        public IActionResult GetMobileVersion()
+        {
+            try
+            {
+                return ApiResult<List<Mobile>>.Ok(
+              DB.GetCollection<Mobile>().Find(a => a.Version != String.Empty).ToList());
+            }
+            catch (Exception e)
+            {
+                return ApiResult<object>.Error(
+                    HttpStatusCode.BadRequest, $"Unable to get version list :\n{Format.ExceptionString(e)}");
+            }
+        }
+
+        [HttpPost("updateandroidversion")]
+        public IActionResult UpdateAndroidVersion([FromForm] TicketForm param)
+        {
+            Mobile mobile = JsonConvert.DeserializeObject<Mobile>(param.JsonData);
+            try
+            {
+                Console.WriteLine("START UPLOAD...");
+                mobile.Upload(Configuration, null, param.FileUpload, x => String.Format("ESSMobile_{0}_{1}", mobile.UpdatedDate, mobile.Version));
+                DB.Save(mobile);
+                return ApiResult<object>.Ok($"Android mobile update has been saved");
+            }
+            catch (Exception e)
+            {
+                return ApiResult<object>.Error(HttpStatusCode.BadRequest, $"Ticket request is failed :\n{e.Message}");
+            }
+        }
+
+        [HttpPost("updateiosversion")]
+        public IActionResult UpdateIOSVersion([FromForm] TicketForm param)
+        {
+            Mobile mobile = JsonConvert.DeserializeObject<Mobile>(param.JsonData);
+            try
+            {
+                mobile.Upload(Configuration, null, param.FileUpload, x => String.Format("ESSMobile{0}{1}", mobile.UpdatedDate, mobile.Version));
+                DB.Save(mobile);
+                return ApiResult<object>.Ok($"IOS mobile update has been saved");
+            }
+            catch (Exception e)
+            {
+                return ApiResult<object>.Error(HttpStatusCode.BadRequest, $"Ticket request is failed :\n{e.Message}");
+            }
+        }
+        [HttpGet("download/{employeeID}")]
+        public IActionResult MDownload(string employeeID)
+        {
+            Mobile result = DB.GetCollection<Mobile>().Find(a => a.Type == employeeID).FirstOrDefault();
+            try
+            {
+                var bytes = result.Download();
+                return File(bytes, "application/force-download", Path.GetFileName(result.Filepath));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         [HttpGet("ping")]
--- KANO.ESS/KANO.Api.Auth/Controllers/AuthController.cs
+++ KANO.ESS/KANO.Api.Auth/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -8,11 +7,14 @@ using KANO.Core.Lib;
 using KANO.Core.Lib.Extension;
 using KANO.Core.Model;
 using KANO.Core.Service;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace KANO.Api.Auth.Controllers
 {
@@ -115,6 +117,81 @@ namespace KANO.Api.Auth.Controllers
             var authService = new AuthService(Mongo, Configuration);
             var result = authService.ChangePassword(param.EmployeeID,param.Password, param.NewPassword);
             return ApiResult<AuthResult>.Ok(result);
+        }
+
+        /**
+         * Function for ESS Mobile because ESS Mobile need Authentication except signin
+         * Every function must authorize with token from signin function
+         * This is for security
+         */
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult SignIn([FromBody] LoginParam param)
+        {
+            try
+            {
+                AuthResult authResult = new AuthService(Mongo, Configuration).Auth(param.EmployeeID, param.Email, param.Password);
+                if (authResult.AuthState == AuthState.Authenticated)
+                {
+                    var claims = new[]{
+                        new Claim(JwtRegisteredClaimNames.UniqueName, authResult.User.Id ),
+                        new Claim(JwtRegisteredClaimNames.Sub, authResult.User.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, authResult.User.FullName),
+                    };
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var expire_minutes = Convert.ToDouble(Configuration["Tokens:ExpireMinutes"]);
+                    var token = new JwtSecurityToken(Configuration["Tokens:Issuer"], Configuration["Tokens:Audience"],
+                                claims,
+                                expires: DateTime.Now.AddMinutes(expire_minutes),
+                                signingCredentials: creds);
+                    authResult.Data = new JwtSecurityTokenHandler().WriteToken(token);
+                    return ApiResult<AuthResult>.Ok(authResult);
+                }
+                else
+                {
+                    return ApiResult<AuthResult>.Error(HttpStatusCode.Unauthorized, authResult.Message);
+                }
+
+            }
+            catch (Exception e) { return ApiResult<AuthResult>.Error(e); }
+        }
+
+        [Authorize]
+        [HttpPost("mchangepassword")]
+        public IActionResult MChangePassword([FromBody] ChangePasswordParam param)
+        {
+            AuthResult auth = new AuthService
+                (Mongo, Configuration).ChangePassword(param.EmployeeID, param.Password, param.NewPassword);
+            if (auth.Success == true)
+            {
+                return ApiResult<AuthResult>.Ok(auth);
+            }
+            return ApiResult<AuthResult>.Error(HttpStatusCode.BadRequest, auth.Message);
+        }
+
+        [Authorize]
+        [Route("mresetpassword/request")]
+        public IActionResult MSendResetPasswordEmail([FromBody] SendResetPasswordEmailParam param)
+        {
+            return ApiResult<AuthResult>.Ok(new AuthService
+                (Mongo, Configuration).SendResetPassword(param.Email));
+        }
+
+        [Authorize]
+        [HttpGet("mismustchangepassword/{token}")]
+        public IActionResult MIsMustChangePassword(string token)
+        {
+            try
+            {
+                DateTime nw = DateTime.Now;
+                DateTime lastChangePassword = Db.GetCollection<User>().Find(a => a.Username == token).FirstOrDefault().LastPasswordChangedDate;
+                int tracehold = Db.GetCollection<Core.Model.Auth.ConfigPassword>().Find(a => a.Published == 1).FirstOrDefault().MustChangeDays;
+                Double diffdays = (nw - lastChangePassword).TotalDays;
+                return ApiResult<AuthResult>.Ok((diffdays >= tracehold) ? "true" : "false");
+            }
+            catch (Exception e) { return ApiResult<AuthResult>.Error(e); }
         }
 
         [HttpGet("ping")]
