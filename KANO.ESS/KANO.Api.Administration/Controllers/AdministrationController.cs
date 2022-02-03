12797ad18f05f986d049628a35d4053152cce295
Dirga <dirgantoro.developer@gmail.com>
2022-02-03 08:59:47 +0700

    Administration service update

--- KANO.ESS/KANO.Api.Administration/Controllers/AdministrationController.cs
+++ KANO.ESS/KANO.Api.Administration/Controllers/AdministrationController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using KANO.Core.Lib;
-using KANO.Core.Lib.Helper;
 using KANO.Core.Model.Auth;
 using KANO.Core.Service;
 using Microsoft.AspNetCore.Mvc;
@@ -21,9 +20,9 @@ namespace KANO.Api.Administration.Controllers
     [Route("api/[controller]")]
     public class AdministrationController : Controller
     {
-        private IMongoManager Mongo;
-        private IMongoDatabase DB;
-        private IConfiguration Configuration;
+        private readonly IMongoManager Mongo;
+        private readonly IMongoDatabase DB;
+        private readonly IConfiguration Configuration;
 
         // Required, this make sure we use Dependency Injection provided by ASP.Core
         public AdministrationController(IMongoManager mongo, IConfiguration conf)
@@ -36,88 +35,60 @@ namespace KANO.Api.Administration.Controllers
         [HttpGet("getconfigpassword")]
         public IActionResult GetConfigPassword()
         {
-            try
-            {
-                return ApiResult<ConfigPassword>.Ok(DB.GetCollection<ConfigPassword>().Find(a => a.Published == 1).FirstOrDefault());
-            }
-            catch (Exception e)
-            {
-                return ApiResult<object>.Error(HttpStatusCode.BadRequest, $"Unable to get config :\n{Format.ExceptionString(e)}");
-            }
+            try { return ApiResult<ConfigPassword>.Ok(DB.GetCollection<ConfigPassword>().Find(a => a.Published == 1).FirstOrDefault());}
+            catch (Exception e) { return ApiResult<object>.Error(HttpStatusCode.BadRequest, e.Message);}
         }
         [HttpPost("updateconfigpassword")]
         public IActionResult UpdateConfigPassword([FromForm] FamilyForm param)
         {
-            try
-            {
+            try {
                 DB.Save(JsonConvert.DeserializeObject<ConfigPassword>(param.JsonData));
-                return ApiResult<object>.Ok($"Config Password Has Been Updated");
-            }
-            catch (Exception e)
-            {
-                return ApiResult<object>.Ok($"Error Update Config Password :\n{e.Message}");
-            }
+                return ApiResult<object>.Ok("success");
+            } catch (Exception e) { return ApiResult<object>.Error(HttpStatusCode.BadRequest, e.Message); }
         }
 
         [HttpGet("getmobileversion")]
         public IActionResult GetMobileVersion()
         {
-            try
-            {
-                return ApiResult<List<Mobile>>.Ok(
-              DB.GetCollection<Mobile>().Find(a => a.Version != String.Empty).ToList());
-            }
-            catch (Exception e)
-            {
-                return ApiResult<object>.Error(
-                    HttpStatusCode.BadRequest, $"Unable to get version list :\n{Format.ExceptionString(e)}");
-            }
+            try { return ApiResult<List<Mobile>>.Ok(DB.GetCollection<Mobile>().Find(a => a.Version != String.Empty).ToList()); }
+            catch (Exception e) { return ApiResult<object>.Error(HttpStatusCode.BadRequest, e.Message);}
         }
 
         [HttpPost("updateandroidversion")]
         public IActionResult UpdateAndroidVersion([FromForm] TicketForm param)
         {
-            Mobile mobile = JsonConvert.DeserializeObject<Mobile>(param.JsonData);
-            try
-            {
-                mobile.Upload(Configuration, null, param.FileUpload, x => String.Format("ESSMobile_{0}_{1}", mobile.UpdatedDate, mobile.Version));
+            
+            try {
+                Mobile mobile = JsonConvert.DeserializeObject<Mobile>(param.JsonData);
+                mobile.Upload(Configuration, null, param.FileUpload, x => String.Format("ESSMobile{0}{1}", mobile.UpdatedDate, mobile.Version));
                 DB.Save(mobile);
-                return ApiResult<object>.Ok($"Android mobile update has been saved");
-            }
-            catch (Exception e)
-            {
-                return ApiResult<object>.Error(HttpStatusCode.BadRequest, $"Ticket request is failed :\n{e.Message}");
-            }
+                return ApiResult<object>.Ok("success");
+            } catch (Exception e) { return ApiResult<object>.Error(HttpStatusCode.BadRequest, e.Message); }
         }
 
         [HttpPost("updateiosversion")]
         public IActionResult UpdateIOSVersion([FromForm] TicketForm param)
         {
-            Mobile mobile = JsonConvert.DeserializeObject<Mobile>(param.JsonData);
-            try
-            {
+            
+            try {
+                Mobile mobile = JsonConvert.DeserializeObject<Mobile>(param.JsonData);
                 mobile.Upload(Configuration, null, param.FileUpload, x => String.Format("ESSMobile{0}{1}", mobile.UpdatedDate, mobile.Version));
                 DB.Save(mobile);
-                return ApiResult<object>.Ok($"IOS mobile update has been saved");
+                return ApiResult<object>.Ok("success");
             }
-            catch (Exception e)
-            {
-                return ApiResult<object>.Error(HttpStatusCode.BadRequest, $"Ticket request is failed :\n{e.Message}");
-            }
+            catch (Exception e) { return ApiResult<object>.Error(HttpStatusCode.BadRequest, e.Message); }
         }
+
         [HttpGet("download/{employeeID}")]
         public IActionResult MDownload(string employeeID)
         {
-            Mobile result = DB.GetCollection<Mobile>().Find(a => a.Type == employeeID).FirstOrDefault();
-            try
-            {
+            
+            try {
+                Mobile result = DB.GetCollection<Mobile>().Find(a => a.Type == employeeID).FirstOrDefault();
                 var bytes = result.Download();
                 return File(bytes, "application/force-download", Path.GetFileName(result.Filepath));
             }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            catch (Exception e) { throw e; }
         }
 
         [HttpGet("ping")]
